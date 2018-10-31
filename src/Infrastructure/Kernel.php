ce9e24b9d690984901aecceaaaeda6f7140bebd5
Bram Van der Sype <bram.vandersype@gmail.com>
2018-10-31 11:10:15 +0100

    Add strict types for kernel and make final

--- src/Infrastructure/Kernel.php
+++ src/Infrastructure/Kernel.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Todo\Infrastructure;
 
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
-class Kernel extends BaseKernel
+final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
