7c06508390cda99155da1a8fd9dd07db37f0fba9
Bram Van der Sype <bram.vandersype@gmail.com>
2018-11-12 21:56:59 +0100

    Put annotation back

--- src/Infrastructure/Controller/UserController.php
+++ src/Infrastructure/Controller/UserController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Todo\Application\User\RegisterUser;
 use Todo\Domain\User\Password;
 use Todo\Domain\User\UserId;
