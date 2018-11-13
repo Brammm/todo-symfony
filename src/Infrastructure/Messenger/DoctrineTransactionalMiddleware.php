88c0e3a2b83231ca00f34724257a5cc42d71d302
Bram Van der Sype <bram.vandersype@gmail.com>
2018-11-13 10:34:04 +0100

    Flush doctrine etc

---
+++ src/Infrastructure/Messenger/DoctrineTransactionalMiddleware.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Todo\Infrastructure\Messenger;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
+
+final class DoctrineTransactionalMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param object $message
+     *
+     * @return mixed
+     */
+    public function handle($message, callable $next)
+    {
+        $this->entityManager->beginTransaction();
+        try {
+            $result = $next($message);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (\Exception $e) {
+            $this->entityManager->rollback();
+        } catch (\Throwable $t) {
+            $this->entityManager->rollback();
+        }
+
+        return $result;
+
+    }
+}
